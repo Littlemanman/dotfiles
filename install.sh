e621e273bae140858d13502946b531e522e3e67c
root2m <lmm@RAZEN-2600>
2022-12-20 22:28:40 +0800

    add zellij config.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 ln -sf $(pwd)/.zshrc ~/.zshrc
+
 ln -sf $(pwd)/.vimrc ~/.vimrc
+
+if [ ! -d ~/.config/zellij ]; then
+  mkdir -p ~/.config/zellij
+fi
+ln -sf $(pwd)/.config/zellij/config.kdl ~/.config/zellij/config.kdl
